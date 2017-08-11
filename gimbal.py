1e3a3aa0b8171d61944c6cfc17ff19ce8b3e2f41
dagord <m.dagord@atcetera.it>
2017-08-11 18:05:31 +0200

    modifiche

--- gimbal.py
+++ gimbal.py
@@ -5,6 +5,7 @@
 import serial
 import time
 import array
+import sys
 
 
 def to_hex(text_string):
@@ -39,6 +40,9 @@ def send_command(parameters):
     beginning_hex = '\x3E'
     command_id_hex = chr(parameters['id'])
 
+    # show command id
+    print "Command id: " + str(parameters['id'])
+
     # send beginning characher
     ser.write(beginning_hex)
     # send command id
@@ -61,24 +65,22 @@ def send_command(parameters):
         body_checksum = 0
         # parse data array
         while ii < data_length:
-            # converts array item to a two-digit hex number, removing initial "0x"
             valore_assoluto = parameters['data'][ii]
             if (parameters['data'][ii] < 0):
                 parameters['data'][ii] = 256 + parameters['data'][ii]
-            # print parameters['data'][ii]
-            # parametro_convertito = format(parameters['data'][ii], '02x').replace("0x", "")
+
             parametro_convertito = chr(parameters['data'][ii] % 256).encode("hex")
 
-            print parametro_convertito
-            data_hex += parametro_convertito + " "
+            # print parametro_convertito
+            data_hex += parametro_convertito
             # increases body checksum
             body_checksum += int(valore_assoluto)
             ii += 1
 
         # send bytearray
+        print "Data: "+str(data_hex)
         ser.write(bytearray.fromhex(data_hex))
-        # ser.write(data_hex)
-        print "Somma dei byte: "+str(body_checksum)
+        print "Total bytes: "+str(body_checksum)
         body_checksum_hex = chr(body_checksum % 256)
 
     else:
@@ -92,6 +94,7 @@ def send_command(parameters):
         header_checksum_hex = chr(header_checksum % 256)
         ser.write(header_checksum_hex)
         ser.write(data_hex)
+        print "No data"
 
     # send body checksum
     ser.write(body_checksum_hex)
@@ -104,15 +107,17 @@ def send_command(parameters):
     # ser.write(comando)
 
     # visualizza a schermo la stringa raw
-    # print "Invio il comando: "+to_hex(comando)
+    print "String sent: "+to_hex(comando)
 
     if 'wait_response' in parameters:
         print "----------------------"
-        print "Attendo la risposta..."
+        print "Waiting response..."
         time.sleep(parameters['wait_response_timeout'])
         risposta = parse_results()
         print "Command id: "+str(risposta['command_id'])
         print "Data "+str(risposta['decoded_data'])
+
+    print
 
 
 CMD_MOTORS_ON = {}
@@ -135,13 +140,14 @@ CMD_READ_PARAMS['wait_response_timeout'] = 0.5
 CMD_BEEP_SOUND = {}
 CMD_BEEP_SOUND['id'] = 89
 CMD_BEEP_SOUND['data'] = [ "80", "00", "7D", "00", "00", "00", "00", "00", "00", "00", "00", "00", "B8", "0B"]
-# CMD_BEEP_SOUND['wait_response'] = True
-# CMD_BEEP_SOUND['wait_response_timeout'] = 0.5
+CMD_BEEP_SOUND['data_format'] = "dec"
 
 CMD_CONTROL = {}
 CMD_CONTROL['id'] = 67
 # CMD_CONTROL['data'] = [ "01", "00", "d8", "00", "48", "00", "00", "00", "00", "00", "00", "00", "00" ]
-CMD_CONTROL['data'] = [ 128,128,128, 0,0,0,1, 0,0,0,1, 0,0,0,1 ]
+CMD_CONTROL['data'] = [ 67,67,67, 0,2,0,2, 0,2,0,2, 0,2,0,2 ]
+CMD_CONTROL['data'] = [ 67,67,67, 0,0,0,8, 0,0,0,0, 0,0,0,0, ]
+CMD_CONTROL['data'] = [ 1,1,1, 0,0,0,0, 0,0,0,0, 0,0,0,0, ]
 # CMD_CONTROL['data_format'] = "hex"
 CMD_CONTROL['wait_response'] = True
 CMD_CONTROL['wait_response_timeout'] = 0.5
@@ -151,6 +157,9 @@ CMD_EXECUTE_MENU = {}
 CMD_EXECUTE_MENU['id'] = 69
 CMD_EXECUTE_MENU['data'] = [18]
 
+CMD_RESET = {}
+CMD_RESET['id'] = 114
+
 CMD_AUTO_PID = {}
 CMD_AUTO_PID['id'] = 35
 CMD_AUTO_PID['data'] = ["01", "01", "ff", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00"]
@@ -159,21 +168,35 @@ CMD_AUTO_PID['data_format'] = "hex"
 # stabilisce la connessione: tenta di riconoscere la porta usb alla quale
 # è connesso (fino a un massimo di 100)
 usb = 0
+connected = 0
 while usb < 100 and usb >= 0:
     try:
         ser = serial.Serial('/dev/ttyUSB'+str(usb))  # open serial port
         usb = -1
+        connected = 1
     except:
         usb += 1
+
+
+if connected == 0:
+    print "Device not connected or not available!"
+    print "(are you running script as superser?)"
+    print
+    sys.exit()
 
 print "Connesso su: "+str(ser.name)
 ser.baudrate = 115200
 
 send_command(CMD_MOTORS_ON)
 time.sleep(1)
-send_command(CMD_EXECUTE_MENU)
-time.sleep(2)
-send_command(CMD_CONTROL)
+# send_command(CMD_EXECUTE_MENU)
+send_command(CMD_RESET)
+time.sleep(5)
+
+print "Device ready to work!"
+# send_command(CMD_CONTROL)
+# send_command(CMD_EXECUTE_MENU)
+# send_command(CMD_CONTROL)
 # time.sleep(10)
 # send_command(CMD_EXECUTE_MENU)
 # time.sleep(2)
